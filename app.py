440042964ae484ebf049e400904b37d6d200a854
Lida Shahmiri <100493593+LShahmiri@users.noreply.github.com>
2025-12-01 13:06:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     answer = None
-
     if request.method == "POST":
         question = request.form.get("question")
         if question:
             result = agent.invoke({"messages": [{"role": "user", "content": question}]})
             answer = result["messages"][-1].content
-
     return render_template("index.html", answer=answer)
-
-if __name__ == "__main__":
-    app.run(debug=True)
